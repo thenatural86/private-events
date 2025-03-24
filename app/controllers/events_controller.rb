cf009adf19f09bbd0901cf6332bb8e22d71c21be
thenatural86 <philip.smalls86@gmail.com>
2025-03-24 17:24:57 -0400

    adds new and create actions for events contoller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,24 @@
 class EventsController < ApplicationController
   def index
-    @events = Events.all
+    @events = Event.all
+  end
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to @event, notice: "Event was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :date, :location, :description)
   end
 end
